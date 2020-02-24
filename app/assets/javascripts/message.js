495c7e0609216147b250ea450140b91597db1893
sentaku-0303 <60691954+sentaku-0303@users.noreply.github.com>
2020-02-24 14:25:21 +0900

    create

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function(){
     var content = message.content ? `<p class="lower-message__content">${message.content}</p>` : "";
     var image = message.image ? `<img class="lower-message__image" src="${message.image}">` : "";
 
-    var html = `<div class="message" data-id=${message.id}>
+    var html = `<div class="message" data-message-id=${message.id}>
                 <div class="upper-message">
                 <div class="upper-message__user-name">
                 ${message.user_name}
@@ -44,4 +44,30 @@ $(function(){
     })
     return false;
   });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: 'api/messages',
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 5000);
+  }
 });
