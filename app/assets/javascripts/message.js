c97b4469fdbc341349e107f8d936d5315b32fc4f
sentaku-0303 <60691954+sentaku-0303@users.noreply.github.com>
2020-02-24 14:39:09 +0900

    create

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -64,7 +64,7 @@ $(function(){
       }
     })
     .fail(function() {
-      console.log('error');
+      alert('reloadMessageError');
     });
   };
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
