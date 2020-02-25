1cdfb37b39bbc59cb92f479155aac1bdcf93a9a7
sentaku-0303 <60691954+sentaku-0303@users.noreply.github.com>
2020-02-25 22:00:34 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.12.0'
 
 # Capistranoのログの表示に利用する
-set :application, 'Chat-Spase'
+set :application, 'ChatSpase'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:sentaku-0303/chat-space.git'
