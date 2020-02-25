53134791ccee96af1fcd8044974442c7b2d52239
sentaku-0303 <60691954+sentaku-0303@users.noreply.github.com>
2020-02-25 21:53:59 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.12.0'
 set :application, 'Chat-Spase'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'sentaku-0303/chat-space.git'
+set :repo_url,  'git@github.com:sentaku-0303/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
